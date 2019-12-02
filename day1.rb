e2a6d244c83c1beaf0618af31800c6e67d50a071
Tim <tim@smartfactory.ch>
2019-12-02 09:58:22 +0100

    adds day 1+2

---
+++ day1.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+fuel=0
+file=File.open("input/day1.txt").read
+file.each_line do |line|
+  fuel += line.to_i / 3 - 2
+end
+
+puts "part 1 fuel is: #{fuel}"
+
+def calcFuel(mass)
+  fuel=mass.to_i / 3 - 2
+  if fuel < 0
+    return 0
+  end
+
+  fuel+=calcFuel(fuel)
+end
+
+fuel_p2=0
+file.each_line do |mass|
+  fuel_p2 += calcFuel(mass.to_i)
+end
+puts "part 2 fuel is: #{fuel_p2}"
---
+++ day2.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/ruby
+
+original=File.open("input/day2.txt").read.split(",").map(&:to_i)
+a = original.dup
+a[1] = 12
+a[2] = 2
+
+position = 0
+while a[position] != 99
+  opcode = a[position]
+
+  if opcode == 1
+    # add
+    a[a[position+3]]=a[a[position+1]]+a[a[position+2]]
+  elsif opcode == 2
+    # mul
+    a[a[position+3]]=a[a[position+1]]*a[a[position+2]]
+  end
+
+  position += 4
+end
+
+
+puts "part 1: #{a[0]}"
+
+def calc(a, noun, verb)
+  a[1] = noun
+  a[2] = verb
+
+  instruction_pointer=0
+  while a[instruction_pointer] != 99
+    instruction = a[instruction_pointer]
+
+    if instruction == 1
+      # add
+      a[a[instruction_pointer+3]]=a[a[instruction_pointer+1]]+a[a[instruction_pointer+2]]
+    elsif instruction == 2
+      # mul
+      a[a[instruction_pointer+3]]=a[a[instruction_pointer+1]]*a[a[instruction_pointer+2]]
+    end
+
+    instruction_pointer += 4
+  end
+
+  a[0]
+end
+
+result=0
+noun=-1 # so it will increase to 1 in the first entry
+verb=0
+max_length=original.length # dont need to increase above that, you wont find a value anyways
+while result != 19690720
+
+  # reset :)
+  noun += 1
+  if noun >= max_length
+    noun = 0
+    verb += 1
+  end
+
+  a = original.dup
+  result = calc(a, noun, verb)
+
+end
+
+puts "day2: #{100 * noun + verb}"
