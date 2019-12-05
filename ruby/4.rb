d1a698bddf4ec8882cfff2197e651b9568e92caa
Tim Jahn <ceoymw@gmail.com>
2019-12-05 15:58:48 +0100

    wip d42

--- ruby/4.rb
+++ ruby/4.rb
@@ -1,8 +1,24 @@
 #!/usr/bin/ruby
 
 file=File.open("../input/day4.txt").read
+regex1 = /^0*1*2*3*4*5*6*7*8*9*$/
+regex2 = /([0-9])\1/
+
+occurences = 0
+(172851..675869).to_a.each { |number|
+  if number.to_s.match(regex1) and number.to_s.match(/([0-9])\1/)
+    occurences += 1
+  end
+}
+
+puts "day 4 part 1: #{occurences}"
 
 
-puts "day 4 part 1: #{}"
+occurences = 0
+(172851..675869).to_a.each { |number|
+  if number.to_s.match(regex1) and number.to_s.match(/([0-9])\1/)
+    occurences += 1
+  end
+}
 
-puts "day 4 part 2: #{}"
+# puts "day 4 part 2: #{}"
